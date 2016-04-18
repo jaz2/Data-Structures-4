1e6cbfcd9a678acbd1f11b6b0512bf09d1f25d9e
Jazmine Zurita <jazzurita1@gmail.com>
2016-04-18 18:28:29 -0400

    File /Users/Jazz/Documents/workspace2/Assignment4/src/FreeBlock.java changed at Mon Apr 18 18:28:29 EDT 2016

---
+++ src/FreeBlock.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jazmine Zurita and Jessica McCready
+ * @version April 18 2016
+ *
+ */
+public class FreeBlock {
+	
+	/**
+	 * The size
+	 */
+	public int sz;
+	
+	/**
+	 * The position
+	 */
+	public int p;
+	
+	/**
+	 * Constructor
+	 * @param size the size of the block
+	 * @param pos where it is
+	 */
+	public FreeBlock(int size, int pos)
+	{
+		sz = size;
+		p = pos;
+	}
+}
