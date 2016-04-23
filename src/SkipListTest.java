a9219faf5ee31bf45e9e466f54b996b83950a2d3
Jazmine Zurita <jazzurita1@gmail.com>
2016-04-23 17:34:29 -0400

    File /Users/Jazz/Documents/workspace2/Assignment4/src/SkipListTest.java changed at Sat Apr 23 17:34:29 EDT 2016

--- src/SkipListTest.java
+++ src/SkipListTest.java
@@ -68,7 +68,7 @@ public class SkipListTest extends TestCase
        assertFuzzyEquals("SkipList dump: \n"
                + "Node has depth 2, Value (null)\n"
                + "Node has depth 2, Value (a, 1, 2, 3, 4)\n"
-               + "Node has depth 2, Value (hey, 1, 2, 12, 4)\n"
+               + "Node has depth 2, Value (b, 1, 2, 3, 4)\n"
                + "SkipList size is: 2\n"
                + "Freelist Blocks: \n(0, 512)", output);
     }
