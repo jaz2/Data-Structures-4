c9491e40731aee258101fcd9bcd19c7ce399be0e
Jazmine Zurita <jazzurita1@gmail.com>
2016-04-25 19:10:43 -0400

    File /Users/Jazz/Documents/workspace2/Assignment4/src/MemoryManager.java changed at Mon Apr 25 19:10:43 EDT 2016

--- src/MemoryManager.java
+++ src/MemoryManager.java
@@ -93,7 +93,7 @@ public class MemoryManager {
         }
         else 
         {
-            byte[] nu = new byte[mm.length + Math.max(sz - 1, bytesNeeded)];
+            byte[] nu = new byte[mm.length + Math.max(sz, bytesNeeded - 1)];
             System.arraycopy(mm, 0, nu, 0, mm.length);
             mm = nu;  
 
