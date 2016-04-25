728bf5195d9d4144acbd426e00cc6652bf700bfa
Jazmine Zurita <jazzurita1@gmail.com>
2016-04-25 19:15:12 -0400

    File /Users/Jazz/Documents/workspace2/Assignment4/src/MemoryManager.java changed at Mon Apr 25 19:15:12 EDT 2016

--- src/MemoryManager.java
+++ src/MemoryManager.java
@@ -94,7 +94,7 @@ public class MemoryManager {
         else 
         {
             byte[] nu = new byte[mm.length + Math.max(sz, bytesNeeded)];
-            System.arraycopy(mm, 0, nu, 0, mm.length - 1);
+            System.arraycopy(mm, 0, nu, 0, mm.length);
             mm = nu;  
 
             ByteBuffer.wrap(mm).putShort(end, (short) b.length);
