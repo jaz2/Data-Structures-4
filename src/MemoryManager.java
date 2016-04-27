fb86e24222b22e0a3bd714fcb1b957f80b78d31c
Jazmine Zurita <jazzurita1@gmail.com>
2016-04-27 10:45:26 -0400

    File /Users/Jazz/Documents/workspace2/Assignment4/src/MemoryManager.java changed at Wed Apr 27 10:45:26 EDT 2016

--- src/MemoryManager.java
+++ src/MemoryManager.java
@@ -97,7 +97,7 @@ public class MemoryManager {
             position = count + 2;
             FreeBlock f = new FreeBlock(bytesNeeded, position);
             
-            freeList.get(position);
+            freeList.get(count + bytesNeeded);
             freeList.remove(fb);
             freeList.insert(f);
             fb = f;
