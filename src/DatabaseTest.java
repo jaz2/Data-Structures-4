4a6979fcdf634a4ad26554fcc8afc673534d2e93
Jazmine Zurita <jazzurita1@gmail.com>
2016-05-01 16:18:29 -0400

    File /Users/Jazz/Documents/workspace2/Assignment4/src/DatabaseTest.java changed at Sun May 01 16:18:29 EDT 2016

--- src/DatabaseTest.java
+++ src/DatabaseTest.java
@@ -1,42 +1,49 @@
-//import static org.junit.Assert.*;
-//import org.junit.Test;
-//import student.TestCase;
-//
-//
-//// -------------------------------------------------------------------------
-///**
-// *  Tests the Database, SkipList
-// *
-// *  @author Jazmine Zurita
-// *  @version Jan 25, 2016
-// */
-//
-//public class DatabaseTest extends TestCase
-//{
-//    /**
-//     * Tests the insert and calls SkipList
-//     */
-//    @Test
-//    public void testInsert()
-//    {
-//        Database d = new Database();
-//        d.skipInsert("a", 1, 1, 3, 5);
-//        assertEquals(, d.skip.head);
-//    }
-//
-//    /**
-//     * Calls the skiplist remove by coord
-//     * @param x the x coord
-//     * @param y the y coord
-//     * @param w the width
-//     * @param h the height
-//     */
-//    @Test
-//    public void skipRemoveCoord(int x, int y, int w, int h)
-//    {
-//        Database db = new Database();
-//        Rect r = new Rect(null, x, y, w, h);
-//        KVPair<String, Rect> p = new KVPair<String, Rect>(null, r);
-//        db.skip.removeByCoord(r);
-//    }
-//}
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import student.TestCase;
+
+
+// -------------------------------------------------------------------------
+/**
+ *  Tests the Database, SkipList
+ *
+ *  @author Jazmine Zurita
+ *  @version Jan 25, 2016
+ */
+
+public class DatabaseTest extends TestCase
+{
+    /**
+     * Tests the insert and calls SkipList
+     * @throws IOException 
+     * @throws ClassNotFoundException 
+     */
+    @Test
+    public void testInsert() throws ClassNotFoundException, IOException
+    {
+        Database d = new Database();
+        d.skipInsert("a", 1, 1, 3, 5);
+        assertEquals(19, d.skip.head);
+    }
+
+    /**
+     * Calls the skiplist remove by coord
+     * @param x the x coord
+     * @param y the y coord
+     * @param w the width
+     * @param h the height
+     * @throws IOException 
+     * @throws ClassNotFoundException 
+     */
+    @Test
+    public void skipRemoveCoord(int x, int y, int w, int h) throws ClassNotFoundException, IOException
+    {
+        Database db = new Database();
+        Rect r = new Rect(null, x, y, w, h);
+        KVPair<String, Rect> p = new KVPair<String, Rect>(null, r);
+        db.skip.removeByCoord(r);
+    }
+}
