2c15f26f858de1524dcbfe9ac41eb4f6bd015b72
Jazmine Zurita <jazzurita1@gmail.com>
2016-05-01 22:23:40 -0400

    File /Users/Jazz/Documents/workspace2/Assignment4/src/SkipNodeTest.java changed at Sun May 01 22:23:38 EDT 2016

--- src/SkipNodeTest.java
+++ src/SkipNodeTest.java
@@ -14,25 +14,24 @@ import org.junit.Test;
  */
 public class SkipNodeTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	/**
-	 * Simple test for this class
-	 */
-	@Test
-	public void testNode() {
-		SkipNode s = new SkipNode(3, 10);
-		SkipNode y = new SkipNode(5, 4);
-		SkipNode x = new SkipNode(3, 10);
-		assertEquals(3, s.element());
-		assertEquals(10, s.getLevel());
-		assertTrue(s.equalss(x));
-		assertFalse(y.equals(s));
-	}
-
+    /**
+     * Simple test for this class
+     */
+    @Test
+    public void testNode() {
+        SkipNode s = new SkipNode(3, 10);
+        SkipNode y = new SkipNode(5, 4);
+        SkipNode x = new SkipNode(3, 10);
+        assertEquals(3, s.element());
+        assertEquals(10, s.getLevel());
+        assertTrue(s.equalss(x));
+        assertFalse(y.equals(s));
+    }
 }
