ea82c1036901a2896b8a02b27f95d0f18ba5199b
Jazmine Zurita <jazzurita1@gmail.com>
2016-05-02 15:58:50 -0400

    File /Users/Jazz/Documents/workspace2/Assignment4/src/FreeBlock.java changed at Mon May 02 15:58:50 EDT 2016

--- src/FreeBlock.java
+++ src/FreeBlock.java
@@ -8,35 +8,35 @@
  *
  */
 public class FreeBlock {
-	
-	/**
-	 * The size of message
-	 */
-	public int sz;
-	
-	/**
-	 * The position
-	 */
-	public int p;
-	
-	/**
-	 * Constructor
-	 * @param size the size of the block
-	 * @param pos where it is
-	 */
-	public FreeBlock(int size, int pos)
-	{
-		sz = size;
-		p = pos;
-	}
-	
-	/**
-	 * Dumps a free block
-	 * For SkipList dump
-	 * @return the string
-	 */
-	public String dump()
-	{	
-	   return "(" + p + ", " + sz + ")\n";
-	}
+    
+    /**
+     * The size of message
+     */
+    public int sz;
+    
+    /**
+     * The position
+     */
+    public int p;
+    
+    /**
+     * Constructor
+     * @param size the size of the block
+     * @param pos where it is
+     */
+    public FreeBlock(int size, int pos)
+    {
+        sz = size;
+        p = pos;
+    }
+    
+    /**
+     * Dumps a free block
+     * For SkipList dump
+     * @return the string
+     */
+    public String dump()
+    {    
+       return "(" + p + ", " + sz + ")\n";
+    }
 }
